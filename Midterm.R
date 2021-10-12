f20d9c33e7bc56f2bb9829f94ff5f097415f541a
krissymchugh <krissy161.8@gmail.com>
2021-10-12 09:10:30 -0400

    Analyzing problemn with percent change code, testing other solutions

--- Midterm.R
+++ Midterm.R
@@ -12,18 +12,32 @@ str(OhioPopASC)
 ## Calculate the percentage change in the population of each Ohio county from the year 2010 to 2019
 ## YEARS == 3 and 12 in the data, respectively) and output a data frame with this information
 PercentChange <-OhioPopASC %>%
-  group_by(YEAR) %>%
+  group_by(COUNTY) %>%
+  filter(YEAR !=1) %>%
+  filter(YEAR !=2) %>%
+  select(YEAR, POPESTIMATE, COUNTY) %>%
+  arrange(desc(COUNTY), .by_group = TRUE) %>%
+  mutate(pct_change = ((lead(POPESTIMATE))-(lag(POPESTIMATE))/(lead(POPESTIMATE))*100))
+view(PercentChange)
+
+PercentChange <-OhioPopASC %>%
+  group_by(COUNTY) %>%
   filter(YEAR !=1) %>%
   filter(YEAR !=2) %>%
   select(YEAR, POPESTIMATE, COUNTY) %>%
   arrange(desc(YEAR), .by_group = TRUE) %>%
-  mutate(pct_change = ((lead(POPESTIMATE))-(lag(POPESTIMATE))/(lead(POPESTIMATE))*100))
+  mutate(pct_change = ((POPESTIMATE(YEAR == 3))-(POPESTIMATE(YEAR == 12))/(POPESTIMATE(YEAR == 3))*100))
 view(PercentChange)
 ##
 ## Calculate the 3 counties with the highest increase in the percentage of total population and the 
 ## 3 with the lowest percentage increase
 PercentChange %>%
+  group_by(COUNTY) %>%
   arrange(desc(pct_change))
+## Here, we can see that the top 3 counties with the highest increase in the percentage of total 
+## population are, respectively, county 47, 33, and 59.
+
+
 
 
 
