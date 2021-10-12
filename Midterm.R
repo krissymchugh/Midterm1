ba9f63bd4319af4eb3438620c911a7a74135de10
krissymchugh <krissy161.8@gmail.com>
2021-10-12 09:58:21 -0400

    added notes in betweeen code chunks.

--- Midterm.R
+++ Midterm.R
@@ -11,25 +11,32 @@ str(OhioPopASC)
 ##
 ## Calculate the percentage change in the population of each Ohio county from the year 2010 to 2019
 ## YEARS == 3 and 12 in the data, respectively) and output a data frame with this information
+##
 Year3 <- OhioPopASC %>%
   group_by(COUNTY) %>%
   filter(YEAR == 3) %>%
   select(COUNTY, POPESTIMATE) %>%
   mutate(initial = POPESTIMATE)
 View(Year3)
-
+##
+## Created a column for the population estimates of each county in the year 2010.
+##
 Year12 <- OhioPopASC %>%
   group_by(COUNTY) %>%
   filter(YEAR == 12) %>%
   select(COUNTY, POPESTIMATE) %>%
   mutate(final = POPESTIMATE) 
 View(Year12)
-
+##
+## Created a column for the population estimates of each county in the year 2019.
+##
 PercentChange <- rbind(Year3, Year12) %>%
   mutate(pct_change = (initial-final)/(final)*100)
 View(PercentChange)
-
-
+##
+## Combined data sets for the initial and final values for calculating the percent change from the year
+## 2010 to 2019.
+##
 ##
 ## Calculate the 3 counties with the highest increase in the percentage of total population and the 
 ## 3 with the lowest percentage increase
